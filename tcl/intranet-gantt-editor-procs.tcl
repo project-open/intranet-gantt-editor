ce0e7430bd5f3569f5280ef929a9b068914c23e3
Frank Bergmann <frank.bergmann@project-open.com>
2015-09-02 14:49:49 +0000

    - Gantt Editor:   Show only for Gantt projects

--- tcl/intranet-gantt-editor-procs.tcl
+++ tcl/intranet-gantt-editor-procs.tcl
@@ -15,6 +15,11 @@ ad_proc -public gantt_editor_portlet {
 } {
     Returns a HTML code with a Gantt editor for the project.
 } {
+    # Only show for GanttProjects
+    if {[im_security_alert_check_integer -location "im_ganttproject_gantt_component" -value $project_id]} { return "" }
+    set project_type_id [util_memoize [list db_string project_type "select project_type_id from im_projects where project_id = $project_id" -default ""]
+    if {![im_category_is_a $project_type_id [im_project_type_gantt]]} { return "" }
+
     # Sencha check and permissions
     if {![im_sencha_extjs_installed_p]} { return "" }
     im_sencha_extjs_load_libraries
