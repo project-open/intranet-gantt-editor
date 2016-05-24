7a4453cb8c042ab70501ce6861813d3430c9b975
Frank Bergmann <frank.bergmann@project-open.com>
2016-05-24 12:57:39 +0000

    - Gantt Chart:   Now showing an error message instead of a blank screen if   executed in a tab in a non-Gantt project

--- tcl/intranet-gantt-editor-procs.tcl
+++ tcl/intranet-gantt-editor-procs.tcl
@@ -18,7 +18,18 @@ ad_proc -public gantt_editor_portlet {
     # Only show for GanttProjects
     if {[im_security_alert_check_integer -location "im_ganttproject_gantt_component" -value $project_id]} { return "" }
     set project_type_id [util_memoize [list db_string project_type "select project_type_id from im_projects where project_id = $project_id" -default ""]]
-    if {![im_category_is_a $project_type_id [im_project_type_gantt]]} { return "" }
+    if {![im_category_is_a $project_type_id [im_project_type_gantt]]} { 
+
+	# Check if this is run from a tab in a non-Gantt project
+	# In this case we need to show an error message instead of a blank screen.
+	set plugin_id [im_opt_val plugin_id]
+	if {"" ne $plugin_id} {
+	    return "<p>This project is not a Gantt project, but of type '[im_category_from_id $project_type_id]'. 
+            <br>Such projects don't have Gantt charts.</p>"
+	} else {
+	    return "" 
+	}
+    }
 
     # Sencha check and permissions
     if {![im_sencha_extjs_installed_p]} { return "" }
