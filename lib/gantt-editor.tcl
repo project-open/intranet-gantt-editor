b7422067902aba792ad7c2cc170c9a5c89f5b21e
aecres <>
2019-04-03 15:56:51 +0000

    - Now fixing tasks with missing start- or end-date   by setting the task to the maximum project start   or end

--- lib/gantt-editor.tcl
+++ lib/gantt-editor.tcl
@@ -54,6 +54,9 @@ db_1row project_info "
 			main_p.project_id = :project_id
 		) t
 "
+if {"" eq $report_start_date} { set report_start_date [db_string now "select now()"] }
+if {"" eq $report_end_date} { set report_end_date [db_string now "select now()"] }
+
 
 # Default material and Unit of Measure: "Default" and "Hour"
 set default_material_id [im_material_default_material_id]
@@ -62,3 +65,40 @@ set default_uom_id [im_uom_hour]
 
 # 9722 = 'Fixed Work' is the default effort_driven_type
 set default_effort_driven_type_id [parameter::get_from_package_key -package_key "intranet-ganttproject" -parameter "DefaultEffortDrivenTypeId" -default "9722"]
+
+
+# ----------------------------------------------------------------
+# Check that all tasks in the project have a reasonable start- and end-date.
+#
+set tasks_with_bad_start_end [db_list tasks_without_start_or_end "
+	select	sub_p.project_id
+	from	im_projects sub_p,
+		im_projects main_p
+	where	main_p.project_id = :project_id and
+		sub_p.tree_sortkey between main_p.tree_sortkey and tree_right(main_p.tree_sortkey) and
+		(sub_p.start_date is null OR sub_p.end_date is null)
+"]
+foreach task_id $tasks_with_bad_start_end {
+    db_1row sub_info "
+	select	parent_id,
+		start_date,
+		end_date
+	from	im_projects
+	where	project_id = :task_id
+    "
+
+    if {"" eq $start_date} {
+        db_dml update_start "update im_projects set start_date = :report_start_date where project_id = :task_id"
+
+	# Write Audit Trail
+	im_audit -object_id $task_id -comment "Fixed empty start_date with default start in Gantt Editor"
+    }
+
+    if {"" eq $end_date} {
+        db_dml update_end "update im_projects set end_date = :report_end_date where project_id = :task_id"
+
+	# Write Audit Trail
+	im_audit -object_id $task_id -comment "Fixed empty end_date with default end in Gantt Editor"
+    }
+}
+
