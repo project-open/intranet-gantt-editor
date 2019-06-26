cf33b1f3604121c521285a517d19dbaac5ce3281
cosine <>
2019-06-26 12:35:24 +0000

    - Added L10n for dependency types

---
+++ www/view/GanttBarPanel.tcl
@@ -0,0 +1,8 @@
+# /packages/intranet-gantt-editor/www/view/GanttBarPanel.tcl
+
+set finish_to_finish_l10n [lang::message::lookup "" intranet-gantt-editor.Finish_to_Finish "Finish-to-Finish"]
+set finish_to_start_l10n [lang::message::lookup "" intranet-gantt-editor.Finish_to_Start "Finish-to-Start"]
+set start_to_finish_l10n [lang::message::lookup "" intranet-gantt-editor.Start_to_Finish "Start-to-Finish"]
+set start_to_start_l10n [lang::message::lookup "" intranet-gantt-editor.Start_to_Start "Start-to-Start"]
+
+
