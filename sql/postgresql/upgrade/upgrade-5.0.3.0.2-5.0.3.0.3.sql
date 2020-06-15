c02e9f73a81470358f513538e4e35d18b6875d41
Frank Bergmann <frank.bergmann@project-open.com>
2020-06-15 17:03:10 +0000

    - Fixed upgrade script

--- sql/postgresql/upgrade/upgrade-5.0.3.0.2-5.0.3.0.3.sql
+++ sql/postgresql/upgrade/upgrade-5.0.3.0.2-5.0.3.0.3.sql
@@ -1,5 +1,5 @@
 -- 5.0.3.0.2-5.0.3.0.3.sql
-SELECT acs_log__debug('/packages/intranet-core/sql/postgresql/upgrade/upgrade-5.0.3.0.2-5.0.3.0.3.sql','');
+SELECT acs_log__debug('/packages/intranet-gantt-editor/sql/postgresql/upgrade/upgrade-5.0.3.0.2-5.0.3.0.3.sql','');
 
 
 
